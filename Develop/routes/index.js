f0f58f9ff66c6a48679c1ce18fcf909b5eb859d5
Michelle Oakley <michellecoder38@gmail.com>
2021-09-13 15:28:24 -0700

    removed duplicate code

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -1,14 +1,17 @@
 const router = require("express").Router();
-const apiRoutes = require("./apiRoutes");
+// const apiRoutes = require("./apiRoutes");
 const path = require("path");
-router.use(apiRoutes);
+// router.use(apiRoutes);
 
-router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-});
+// router.get("/", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../public/index.html"));
+// });
 
 router.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/status.html"));
 });
 
 
