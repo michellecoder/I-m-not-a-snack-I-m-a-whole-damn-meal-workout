0247e1bc380e3cb5dd94c2976078097598140fd8
Michelle Oakley <michellecoder38@gmail.com>
2021-09-13 12:54:05 -0700

    intial set up of routes and models

---
+++ Develop/routes/index.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+const apiRoutes = require("./apiRoutes");
+const path = require("path");
+router.use(apiRoutes);
+
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+
+
+
+
+
+
+
+
+module.exports = router;
